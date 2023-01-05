db726cdafeed0aaaa58a07e3f58ca1725e42ef4c
Wilbert Iglesias <ubuntu@ip-172-31-1-156.ec2.internal>
2023-01-05 16:44:15 +0000

    cleanup misplaced func

--- src/translate.py
+++ src/translate.py
@@ -21,20 +21,3 @@ def translate(event, context):
                            cls=decimalencoder.DecimalEncoder)
     }
     return response
-
-
-def get(event, context):
-    # create a response
-    item = todoList.get_item(event['pathParameters']['id'])
-    if item:
-        response = {
-            "statusCode": 200,
-            "body": json.dumps(item,
-                               cls=decimalencoder.DecimalEncoder)
-        }
-    else:
-        response = {
-            "statusCode": 404,
-            "body": ""
-        }
-    return response
