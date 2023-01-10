a3f49b2196d9620feb9f65a3d163d17230cd0ca2
Wilbert Iglesias <ubuntu@ip-172-31-1-156.ec2.internal>
2023-01-10 20:58:36 +0000

    commented ENDPOINT_OVERRIDE var reset

--- test/unit/TestToDo.py
+++ test/unit/TestToDo.py
@@ -123,7 +123,7 @@ class TestDatabaseFunctions(unittest.TestCase):
         print('---------------------')
         print('Start: test_get_table')
         from src.todoList import get_table
-        os.environ["ENDPOINT_OVERRIDE"] = ""
+#        os.environ["ENDPOINT_OVERRIDE"] = ""
         table_name = get_table()
         self.assertIsNotNone(table_name)
         print(f"Table name is {table_name}")
